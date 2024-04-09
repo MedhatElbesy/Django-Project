63a263212c97fa513410ab43d13d15d4146eaebb
MedhatElbesy <medhatelbesy@gmail.com>
2024-04-09 19:45:22 +0200

    payment app

---
+++ Backend/payments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from payments.views import all_payments,payment_show,create_payment
+
+urlpatterns = [
+    # path('all/',all_payments,name='all.payments'),
+    # path('show/<int:id>',payment_show,name='payment.show'),
+    # path('create/',create_payment,name='create.payment'),
+]
