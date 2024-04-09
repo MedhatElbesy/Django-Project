a81eb7522ea6815b76b93b61997e45c2e446bc49
Ahmed Ghoraba <ahmed.12.2011@gmail.com>
2024-04-09 15:54:44 +0200

    Added Databse connections, registered accounts, projects apps

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'projects.apps.ProjectsConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crowdsource',
+        'USER': 'iticorwdsource',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
