d9995db25da7253930fab0314e91661cd0f60e3f
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-04-09 21:14:45 +0200

    urls

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('payment/',include('payments.urls'))
-]
+    path('payment/',include('payments.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('users/', include('accounts.urls')),]
