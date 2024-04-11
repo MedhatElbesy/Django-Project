8fb2d43925258177e80f0584b4782aa2781626ac
MedhatElbesy <medhatelbesy@gmail.com>
2024-04-11 02:53:30 +0200

    accept changes

--- Backend/payments/models.py
+++ Backend/payments/models.py
@@ -13,21 +13,14 @@ class PaymentStatus(models.TextChoices):
 
 
 class Payment(models.Model):
-<<<<<<< HEAD
-    project = models.OneToOneField(
-        Project, on_delete=models.CASCADE, related_name="projectrelated")
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name="accountrelated")
-=======
-
-    project = models.OneToOneField(Project, on_delete=models.CASCADE,null=True, related_name="projectrelated")
-    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True, related_name="accountrelated")
->>>>>>> d8dbe851983f27d23da3937990c6cd00b0655c27
+    project = models.OneToOneField(Project, on_delete=models.CASCADE, related_name="projectrelated")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="accountrelated")
     amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=10)
     status = models.CharField(max_length=20, choices=PaymentStatus.choices, default=PaymentStatus.PENDING)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
+
     def __str__(self):
-        return f"Payment for {self.roject} by {self.user}"
+        return f"Payment for {self.project} by {self.user}"
--- Backend/payments/urls.py
+++ Backend/payments/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('create/', PaymentViewSet.as_view({'post': 'create'}), name='createPayment'),
     path('show/<int:id>', PaymentViewSet.as_view({'get': 'show'}), name='showPayment'),
     path('list/', PaymentViewSet.as_view({'get': 'list'}), name='listByProject'),
-    path('listByProject/<int:id>', PaymentViewSet.as_view({'get': 'list_by_project'}), name='createPayment'),
+    path('listByProject/<int:id>', PaymentViewSet.as_view({'get': 'list_by_project'}), name='listByProject'),
     path('listByUser/<int:id>', PaymentViewSet.as_view({'get': 'list_by_user'}), name='listByUser'),
 ]
