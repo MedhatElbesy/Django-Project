12cdfc4454f5dc830c6b4e31496b9b2eca55bc7c
MedhatElbesy <medhatelbesy@gmail.com>
2024-04-13 22:49:24 +0200

    test component

--- Frontend/snabel/src/App.vue
+++ Frontend/snabel/src/App.vue
@@ -1,11 +1,26 @@
 <template>
+<div>
+
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+  <card/>
+</div>
 </template>
 
+<script>
+import card from './components/cardComponent'
+export default {
+  data:()=>({
+    }),
+  methods:{},
+  components:{
+    card,
+  }
+}
+</script>
 <style>
 
 </style>
---
+++ Frontend/snabel/src/components/cardComponent.vue
@@ -0,0 +1,49 @@
+<template {{test()}}>
+    <div class="card" style="width: 18rem;">
+    <img src="" class="card-img-top" alt="">
+    <div class="card-body">
+        <h5 class="card-title"> title</h5>
+        <p class="card-text">{{name}}</p>
+        <input type="range"  default value="10.25">
+        <p class="card-text">mony</p>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data:()=>({
+        // all:[],
+        name:''
+
+    }),
+    methods:{
+        // async test(){
+        //     let data = await fetch('http://127.0.0.1:8000/payment/list/2');
+        //     let jsonData = await data.json()
+        //     console.log(jsonData);
+        //     // this.all=jsonData;
+        //     this.name=jsonData.status;
+        
+        // }
+    },
+    async created(){
+        try{
+            let data = await fetch('http://127.0.0.1:8000/payment/list/2',{
+                method:"GET",
+                headers:{
+                    "Content-Type":"application/json",
+                }
+            });
+            let jsonData = await data.json();
+            console.log(jsonData);
+        }catch(e){
+            console.log(e);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
