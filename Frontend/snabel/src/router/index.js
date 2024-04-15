f21807b23696cde2a41b18d6e2d1e82915ba973d
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-04-15 16:19:02 +0200

    added a route to test development

--- Frontend/snabel/src/router/index.js
+++ Frontend/snabel/src/router/index.js
@@ -1,5 +1,5 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 // import sliderComponent from '../components/sliderComponent.vue'
 // import lastProjectComponent from '../components/lastProjectComponent.vue'
 import RegisterComponent from "../components/Auth/RegisterComponent.vue";
@@ -7,50 +7,56 @@ import LoginComponent from "@/components/Auth/LoginComponent.vue";
 import ForgetPasswordComponent from "@/components/Auth/ForgetPasswordComponent.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/search",
+    name: "search",
+    component: () => import("../components/searchComponent.vue"),
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ "../views/AboutView.vue");
     },
-    {
-        path: '/search',
-        name: 'search',
-        component: ()=>import('../components/searchComponent.vue')
-    },
-    {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: function () {
-            return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-        }
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: RegisterComponent,
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: LoginComponent
-    },
-    {
-        path: '/forget-password',
-        name: 'ForgetPassword',
-        component: ForgetPasswordComponent
-    },
-    {
-        path: '/:cathcAll(.*)',
-        component: ()=> import('../views/AboutView.vue'),
-    }
-]
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: RegisterComponent,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginComponent,
+  },
+  {
+    path: "/forget-password",
+    name: "ForgetPassword",
+    component: ForgetPasswordComponent,
+  },
+  {
+    path: "/projectTest/:id",
+    name: "ProjectTest",
+    component: () => import("../views/projectView.vue"),
+    props: true,
+  },
+  {
+    path: "/:cathcAll(.*)",
+    component: () => import("../views/AboutView.vue"),
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
