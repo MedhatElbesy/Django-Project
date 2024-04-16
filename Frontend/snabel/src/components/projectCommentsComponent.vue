622ab6bcf67b86e0a45e18dde625138bc654bc6a
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-04-16 22:33:00 +0200

    creating comment component

---
+++ Frontend/snabel/src/components/projectCommentsComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- <h1>hi</h1> -->
+  <section class="comments-container">
+    <div
+      class="comment row align-baseline"
+      v-for="comment in comments"
+      :key="comment.id"
+    >
+      <div class="col-2">
+        <img
+          src="https://images.unsplash.com/photo-1500648735779-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+      </div>
+      <div class="col-10">
+        <h3>{{ comment.user_name }}</h3>
+        <p>{{ comment.comment }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+// import { useProjectStore } from "@/stores/project";
+export default {
+  data() {
+    return {
+      comments: [],
+    };
+  },
+  methods: {},
+  computed: {},
+  async created() {
+    // const store = useProjectStore();
+    // const currentProjectID = store.projectID;
+    const response = await fetch("http://localhost:8000/comments/project/1");
+    console.log("**************************************************");
+    console.log(response);
+    if (response.ok) {
+      const data = await response.json();
+      this.comments = data;
+      console.log(data);
+    }
+  },
+  mounted() {},
+  watch: {},
+  components: {},
+  props: {},
+  emits: {},
+};
+</script>
+
+<style></style>
--- Frontend/snabel/src/views/projectPayment.vue
+++ Frontend/snabel/src/views/projectPayment.vue
@@ -1,54 +1,65 @@
 <template>
-    <div>
-        <nav class="row g-0">
-            <navbar />
-        </nav>
-        <section class="d-flex">
-            <div class="border border-2 border-danger col-8 medhat">
-
-            </div>
-            <Suspense>
-            <template #default>
-            <projectDonate/>
-            </template>
-            <template #fallback>
+  <div class="w-100">
+    <nav class="row g-0">
+      <navbar />
+    </nav>
+    <section class="d-flex row mx-auto" style="width: 90%">
+      <h1 class="col-12 my-5">
+        {{ project.title }}
+      </h1>
+      <div class="border border-2 border-danger col-8 medhat">
+        <projectCommentsComponent />
+      </div>
+      <div class="col-4">
+        <Suspense>
+          <template #default>
+            <projectDonate />
+          </template>
+          <template #fallback>
             <div>Loading...</div>
-            </template>
+          </template>
         </Suspense>
-        </section>
-        <card/>
-        <pay/>
-        <footer class="row g-0 bg-light">
-            <footerComponent />
-        </footer>
-    </div>
-    
+      </div>
+    </section>
+    <card />
+    <pay />
+
+    <footer class="row g-0 bg-light">
+      <footerComponent />
+    </footer>
+  </div>
 </template>
 
 <script>
-import navbar from "../components/navComponent.vue"
-import projectDonate from "../components/projectDonationCards.vue"
-import card from "../components/cardComponent.vue"
-import pay from "../components/paymentFormComponent.vue"
-import footerComponent from "../components/footerComponent.vue"
+import navbar from "../components/navComponent.vue";
+import projectDonate from "../components/projectDonationCards.vue";
+import card from "../components/cardComponent.vue";
+import pay from "../components/paymentFormComponent.vue";
+import footerComponent from "../components/footerComponent.vue";
+import projectCommentsComponent from "@/components/projectCommentsComponent.vue";
+import { useProjectStore } from "@/stores/project"; // Import the project store
+
 export default {
-    data:()=>({
-        
-
-    }),
-    components:{
-        navbar,
-        projectDonate,
-        card,
-        pay,
-        footerComponent
-    },
-
-}
+  data: () => ({
+    project: {},
+  }),
+  components: {
+    navbar,
+    projectDonate,
+    card,
+    pay,
+    footerComponent,
+    projectCommentsComponent,
+  },
+  created() {
+    const project = useProjectStore().projectData;
+    this.project = project;
+  },
+};
 </script>
 
 <style>
-.medhat{
-    height: 200px;
+.medhat {
+  height: 200px;
 }
 </style>
