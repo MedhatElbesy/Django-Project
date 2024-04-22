673910b3736f9161d21fbea88a70fb0ea1057c04
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-04-22 03:33:27 +0200

    redirect to profile

--- Frontend/snabel/src/stores/auth.js
+++ Frontend/snabel/src/stores/auth.js
@@ -1,47 +1,48 @@
-import { defineStore } from "pinia";
+import {defineStore} from "pinia";
+import router from "@/router";
 
 export const useAuthenticationStore = defineStore("authenticationStore", {
-  state: () => ({
-    user: {},
-    errorMessages: '',
-    successMessages: '',
-  }),
+    state: () => ({
+        user: {},
+        errorMessages: '',
+        successMessages: '',
+    }),
 
-  actions: {
-    async login(email, password) {
-      console.log(email,password)
-      try {
-        const formData = new FormData();
-        formData.append('email', email);
-        formData.append('password', password);
+    actions: {
+        async login(email, password) {
+            console.log(email, password)
+            try {
+                const formData = new FormData();
+                formData.append('email', email);
+                formData.append('password', password);
 
-        let response = await fetch(`http://localhost:8000/api/login/`, {
-          method: "POST",
-          body: formData
-        });
+                let response = await fetch(`http://localhost:8000/api/login/`, {
+                    method: "POST",
+                    body: formData
+                });
 
-        if (response.ok) {
-          const userData = await response.json();
-          this.user = userData;
-          localStorage.setItem('token', userData.token);
-          this.successMessages = userData.message;
-          this.errorMessages = '';
-          console.log("Login successful:", this.user, userData.message);
-        } else {
-          this.errorMessages = "Login Failed, error in email or password!"
-          console.error("Login failed:", response.status, response.statusText);
+                if (response.ok) {
+                    const userData = await response.json();
+                    localStorage.setItem('token', userData.token);
+                    this.successMessages = userData.message;
+                    this.errorMessages = '';
+                    console.log(this.user);
+                    router.push('/profile');
+                } else {
+                    this.errorMessages = "Login Failed, error in email or password!"
+                    console.error("Login failed:", response.status, response.statusText);
+                }
+            } catch (error) {
+                console.error("Error during login:", error);
+                this.errorMessages = error;
+                this.successMessages = '';
+            }
+        },
+
+
+        logout() {
+            localStorage.removeItem('token');
+            this.user = {};
         }
-      } catch (error) {
-        console.error("Error during login:", error);
-        this.errorMessages = error;
-        this.successMessages = '';
-      }
     },
-
-
-    logout() {
-      localStorage.removeItem('token');
-      this.user = {};
-    }
-  },
 });
