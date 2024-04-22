890f086966959c432041abbd4f20696641d784cc
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-04-22 18:08:21 +0200

    Superuser only access routes of dashboard

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import user_passes_test
 
 
 urlpatterns = [
@@ -33,8 +34,8 @@ urlpatterns = [
     path('ratings/', include('rating.urls')),
 
     # for DashBorad Routes
-    path('reports/', include('reports.dashboardUrls')),
-    path('dashboard/projects/', include('projects.urls')),
-    path('dashboard/categories/', include('categories.urls')),
-    path('dashboard/tags/', include('tags.urls')),
+    path('reports/', user_passes_test(lambda user: user.is_superuser)(include('reports.dashboardUrls'))),
+    path('dashboard/projects/', user_passes_test(lambda user: user.is_superuser)(include('projects.urls'))),
+    path('dashboard/categories/', user_passes_test(lambda user: user.is_superuser)(include('categories.urls'))),
+    path('dashboard/tags/', user_passes_test(lambda user: user.is_superuser)(include('tags.urls'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
